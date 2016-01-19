19313d4ec1df6b8aca63338cd0ff12a35c5ff456
Owain Lewis <owain@owainlewis.com>
2016-01-19 14:06:41 +0000

    Update IO.hs

--- src/Network/Daenerys/IO.hs
+++ src/Network/Daenerys/IO.hs
@@ -1,11 +1,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Network.Daenerys.IO
   ( readFrom
-    -- Read a single request from file
   , readRequest
-    -- Read a list of requests from file
   , readRequests
-    -- Util functions
   , tryFileOr
   ) where
 
@@ -16,13 +13,9 @@ import qualified Data.ByteString.Lazy   as B
 import           Network.Daenerys.Types (InternalRequest)
 import           System.IO.Error        (isDoesNotExistError)
 
--- | Try reading a file and returning the contents or return a default string
---
 tryFileOr :: FilePath -> String -> IO String
 tryFileOr f d = readFile f `catch` (\(e :: IOException) -> return d)
 
--- | Try opening a file and decoding the JSON.
---   If the file does not exist return Nothing else Just (contents)
 readFrom :: FromJSON a => FilePath -> IO (Maybe a)
 readFrom file = do
     e <- tryJust (guard . isDoesNotExistError) (B.readFile file)
@@ -31,11 +24,9 @@ readFrom file = do
       Right contents -> return . decode $ contents
 
 -- | Read a single request
---
 readRequest :: FilePath -> IO (Maybe InternalRequest)
 readRequest = readFrom
 
 -- | Read a list of requests
---
 readRequests :: FilePath -> IO (Maybe [InternalRequest])
 readRequests = readFrom
